6583b3578f99078072bc70115000e9ec834a0b5d
Paulucha <pauluchp@gmail.com>
2018-05-12 13:03:31 +0200

    zmieniono wyswietlanie zwyciezcow

--- ud282-master/Project 1 - TicTacToe/src/main/java/com/udacity/Game.java
+++ ud282-master/Project 1 - TicTacToe/src/main/java/com/udacity/Game.java
@@ -151,6 +151,76 @@ public class Game {
     public String checkGameWinner(char [][]grid){
         String result = "None";
         //Student code goes here ...
+        if (grid [0][2]=='x' && grid [1][1]=='x' && grid [2][0]=='x')
+        {
+            result = "X wins";
+        }
+        else if (grid [0][0]=='x' && grid [1][0]=='x' && grid [2][0]=='x')
+        {
+            result = "X wins";
+        }
+        else if (grid [0][1]=='x' && grid [1][1]=='x' && grid [2][1]=='x')
+        {
+            result = "X wins";
+        }
+        else if (grid [0][2]=='x' && grid [1][2]=='x' && grid [2][2]=='x')
+        {
+            result = "X wins";
+        }
+        else if (grid [0][0]=='x' && grid [0][1]=='x' && grid [0][2]=='x')
+        {
+            result = "X wins";
+        }
+        else if (grid [1][0]=='x' && grid [1][1]=='x' && grid [1][2]=='x')
+        {
+            result = "X wins";
+        }
+        else if (grid [2][0]=='x' && grid [2][1]=='x' && grid [2][2]=='x')
+        {
+            result = "X wins";
+        }
+        else if (grid [0][0]=='x' && grid [1][1]=='x' && grid [2][2]=='x')
+        {
+            result = "X wins";
+        }
+
+        /**
+         * OOOOOO
+         */
+       else if (grid [0][2]=='o' && grid [1][1]=='o' && grid [2][0]=='o')
+        {
+            result = "o wins";
+        }
+        else if (grid [0][0]=='o' && grid [1][0]=='o' && grid [2][0]=='o')
+        {
+            result = "o wins";
+        }
+        else if (grid [0][1]=='o' && grid [1][1]=='o' && grid [2][1]=='o')
+        {
+            result = "o wins";
+        }
+        else if (grid [0][2]=='o' && grid [1][2]=='o' && grid [2][2]=='o')
+        {
+            result = "o wins";
+        }
+        else if (grid [0][0]=='o' && grid [0][1]=='o' && grid [0][2]=='o')
+        {
+            result = "o wins";
+        }
+        else if (grid [1][0]=='o' && grid [1][1]=='o' && grid [1][2]=='o')
+        {
+            result = "o wins";
+        }
+        else if (grid [2][0]=='o' && grid [2][1]=='o' && grid [2][2]=='o')
+        {
+            result = "o wins";
+        }
+        else if (grid [0][0]=='o' && grid [1][1]=='o' && grid [2][2]=='o')
+        {
+            result = "o wins";
+        }
+
+        //Student code ends here
         return result;
     }
 
